0971a23d09b444073905da3393d01a7bd2b30e85
leolsbufalo <leonardolsbufalo@gmail.com>
2020-09-21 15:58:09 -0300

    Costumer converter test

---
+++ src/test/java/com/leolsbufalo/moon/advices/CostumerConverterTest.java
@@ -0,0 +1,67 @@
+package com.leolsbufalo.moon.advices;
+
+import com.leolsbufalo.moon.converter.CostumerConverter;
+import com.leolsbufalo.moon.entity.Costumer;
+import com.leolsbufalo.moon.model.CostumerModel;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CostumerConverterTest {
+
+    @Test
+    public void CostumerConverterToEntityTest() {
+
+        Costumer costumerEntity = costumerEntityMock();
+
+        Costumer convertedCostumer = new CostumerConverter().convertFromDto(costumerModelMock());
+
+        assertEquals(convertedCostumer.getName(), costumerEntity.getName(), "name must be equal");
+        assertEquals(convertedCostumer.getEmail(), costumerEntity.getEmail(), "email must be equal");
+        assertEquals(convertedCostumer.getPhoneCountryCode(), costumerEntity.getPhoneCountryCode(), "Phone country must be equal");
+        assertEquals(convertedCostumer.getPhoneAreaCode(), costumerEntity.getPhoneAreaCode(), "Area code must be equal");
+        assertEquals(convertedCostumer.getPhoneNumber(), costumerEntity.getPhoneNumber(), "phone number must be equal");
+        assertEquals(convertedCostumer.getBirthDate(), costumerEntity.getBirthDate(), "birthday must be equal");
+        assertEquals(convertedCostumer.getDocumentNumber(), costumerEntity.getDocumentNumber(), "document number must be equal");
+    }
+
+    @Test
+    public void CostumerConverterToModelTest() {
+
+        CostumerModel costumerModel = costumerModelMock();
+
+        CostumerModel convertedCostumerModel = new CostumerConverter().convertFromEntity(costumerEntityMock());
+
+        assertEquals(convertedCostumerModel.getName(), costumerModel.getName(), "name must be equal");
+        assertEquals(convertedCostumerModel.getEmail(), costumerModel.getEmail(), "email must be equal");
+        assertEquals(convertedCostumerModel.getPhoneCountryCode(), costumerModel.getPhoneCountryCode(), "Phone country must be equal");
+        assertEquals(convertedCostumerModel.getPhoneAreaCode(), costumerModel.getPhoneAreaCode(), "Area code must be equal");
+        assertEquals(convertedCostumerModel.getPhoneNumber(), costumerModel.getPhoneNumber(), "phone number must be equal");
+        assertEquals(convertedCostumerModel.getBirthDate(), costumerModel.getBirthDate(), "birthday must be equal");
+        assertEquals(convertedCostumerModel.getDocumentNumber(), costumerModel.getDocumentNumber(), "document number must be equal");
+
+    }
+
+
+    public CostumerModel costumerModelMock() {
+        return new CostumerModel(
+                "Charlie de Luca",
+                "charlie@cats.com",
+                55,
+                11,
+                931902304,
+                "03/03/03",
+                "06200450005");
+    }
+
+    public Costumer costumerEntityMock() {
+        return new Costumer(
+                "Charlie de Luca",
+                "charlie@cats.com",
+                55,
+                11,
+                931902304,
+                "03/03/03",
+                "06200450005");
+    }
+
+}
