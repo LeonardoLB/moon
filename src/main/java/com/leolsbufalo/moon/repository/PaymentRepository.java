af3d12153b7cb387593c51aecde26bc63d0e5598
leolsbufalo <leonardolsbufalo@gmail.com>
2020-09-25 15:03:24 -0300

    put repository annotation

--- src/main/java/com/leolsbufalo/moon/repository/PaymentRepository.java
+++ src/main/java/com/leolsbufalo/moon/repository/PaymentRepository.java
@@ -2,7 +2,9 @@ package com.leolsbufalo.moon.repository;
 
 import com.leolsbufalo.moon.entity.Payment;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepository extends CrudRepository<Payment, Integer> {
 
     Payment save(Payment payment);
