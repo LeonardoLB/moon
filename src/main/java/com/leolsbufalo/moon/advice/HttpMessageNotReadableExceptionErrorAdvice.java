cc15ac80cad6f99fdb2ccb44a74547d3f5fa40bf
leolsbufalo <leonardolsbufalo@gmail.com>
2020-09-28 16:02:07 -0300

    try resolve with JsonParseException

--- src/main/java/com/leolsbufalo/moon/advice/HttpMessageNotReadableExceptionErrorAdvice.java
+++ src/main/java/com/leolsbufalo/moon/advice/HttpMessageNotReadableExceptionErrorAdvice.java
@@ -1,5 +1,6 @@
 package com.leolsbufalo.moon.advice;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.leolsbufalo.moon.model.ErrorObject;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,8 +21,7 @@ public class HttpMessageNotReadableExceptionErrorAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     protected ResponseEntity httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException exception) {
-        JsonMappingException jsonMappingException = (JsonMappingException) exception.getCause();
-        List<ErrorObject> errors = getErrors(exception, jsonMappingException);
+        List<ErrorObject> errors = getErrors(exception);
         ErrorResponse errorResponse = getErrorResponse(exception, HttpStatus.BAD_REQUEST, errors);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
@@ -30,9 +31,15 @@ public class HttpMessageNotReadableExceptionErrorAdvice {
                 status.getReasonPhrase(), ex.getClass().getSimpleName(), errors);
     }
 
-    private List<ErrorObject> getErrors(HttpMessageNotReadableException exception, JsonMappingException jsonMappingException) {
-        return List.of( new ErrorObject(
-                "Parse Error, wrong type sent",
-                jsonMappingException.getPath().get(0).getFieldName()));
+    private List<ErrorObject> getErrors(HttpMessageNotReadableException exception) {
+        try {
+            JsonMappingException jsonMappingException = (JsonMappingException) exception.getCause();
+            return List.of(new ErrorObject("Parse Error, wrong type sent",
+                    jsonMappingException.getPath().get(0).getFieldName()));
+        } catch (Exception ex) {
+            JsonParseException jsonParseException = (JsonParseException) exception.getCause();
+            return List.of(new ErrorObject("Parse Error, wrong type sent, verify field description",
+                    jsonParseException.getMessage() ));
+        }
     }
 }
