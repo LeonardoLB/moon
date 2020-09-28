57a3cfc446065991cfbdfa6e321eb6222c100b33
leolsbufalo <leonardolsbufalo@gmail.com>
2020-09-28 09:11:01 -0300

    changed controller advice annotation and renamed file

--- src/main/java/com/leolsbufalo/moon/advice/HttpMessageNotReadableExceptionErrorAdvice.java
+++ src/main/java/com/leolsbufalo/moon/advice/HttpMessageNotReadableExceptionErrorAdvice.java
@@ -8,14 +8,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class InvalidFormatExceptionErrorAdvice {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
