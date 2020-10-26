db4c21a5deddf89551865bb739df66848617a4dc
ln0rd <leonardolsbufalo@gmail.com>
2020-10-26 12:06:41 -0300

    [fix] adjust bean annotation and class string serialize

--- src/main/java/com/leolsbufalo/moon/kafka/KafkaConf.java
+++ src/main/java/com/leolsbufalo/moon/kafka/KafkaConf.java
@@ -3,6 +3,9 @@ package com.leolsbufalo.moon.kafka;
 import com.leolsbufalo.moon.entity.Payment;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Properties;
@@ -12,11 +15,12 @@ public class KafkaConf {
 
     Properties props = new Properties();
 
-    public KafkaProducer config(Payment payment) {
+    @Bean
+    public KafkaProducer config() {
 
         props.put("bootstrap.servers", "moon-kafka:9092");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("key.serializer", IntegerSerializer.class);
+        props.put("value.serializer", StringSerializer.class);
 
         return new KafkaProducer(props);
     }
