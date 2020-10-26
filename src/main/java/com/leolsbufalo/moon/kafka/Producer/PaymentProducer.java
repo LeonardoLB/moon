b60adb7d5392b5b14dae8ae293e22bd61a033b4f
ln0rd <leonardolsbufalo@gmail.com>
2020-10-26 14:19:20 -0300

    [feat] try in service impl

--- src/main/java/com/leolsbufalo/moon/kafka/Producer/PaymentProducer.java
+++ src/main/java/com/leolsbufalo/moon/kafka/Producer/PaymentProducer.java
@@ -2,6 +2,7 @@ package com.leolsbufalo.moon.kafka.Producer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.leolsbufalo.moon.entity.Payment;
+import com.leolsbufalo.moon.exception.KafkaSenderException;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +22,12 @@ public class PaymentProducer {
     public Payment send(Payment payment) {
         ProducerRecord<Integer, String> producerRecord = new ProducerRecord<>(createPaymentTopic, payment.getId(), payment.toJson());
 
-        paymentProducer.send(producerRecord);
-        paymentProducer.flush();
-        paymentProducer.close();
-        return payment;
+        try {
+            paymentProducer.send(producerRecord);
+            paymentProducer.flush();
+            return payment;
+        } catch (Exception exception) {
+            throw exception;
+        }
     }
 }
