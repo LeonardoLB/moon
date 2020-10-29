d8cd8511a840bd1c2a5a06983c9d0f940af3f29a
ln0rd <leonardolsbufalo@gmail.com>
2020-10-28 23:05:48 -0300

    [feat] new propertie in payment mock

--- src/test/java/com/leolsbufalo/moon/service/PaymentServiceTest.java
+++ src/test/java/com/leolsbufalo/moon/service/PaymentServiceTest.java
@@ -51,6 +51,7 @@ public class PaymentServiceTest {
 
     public static Payment createPaymentEntity() {
         return new Payment(
+                        "12345678910",
                 "BRL",
                 1050,
                 1,
