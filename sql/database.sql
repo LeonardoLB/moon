c778486af4333bb4bef9814eb1e1ed40774bc3ce
ln0rd <leonardolsbufalo@gmail.com>
2020-10-30 16:00:50 -0300

    [fix] changed tables primary keys

--- sql/database.sql
+++ sql/database.sql
@@ -1,46 +1,8 @@
--- MySQL Workbench Synchronization
--- Generated: 2020-08-19 11:34
--- Model: New Model
--- Version: 1.0
--- Project: Name of the project
--- Author: Bufalo
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 CREATE SCHEMA IF NOT EXISTS `moon` DEFAULT CHARACTER SET utf8 ;
-
-CREATE TABLE IF NOT EXISTS `moon`.`payment` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `unique_id` VARCHAR(200) NOT NULL,
-  `currency` VARCHAR(45) NOT NULL,
-  `value_in_cents` INT(11) NOT NULL,
-  `installments` INT(11) NOT NULL,
-  `costumer_id` INT(11) NOT NULL,
-  `costumer_andress_id` INT(11) NOT NULL,
-  `costumer_card_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_payment_costumer_idx` (`costumer_id` ASC),
-  INDEX `fk_payment_costumer_andress1_idx` (`costumer_andress_id` ASC),
-  INDEX `fk_payment_costumer_card1_idx` (`costumer_card_id` ASC),
-  CONSTRAINT `fk_payment_costumer`
-    FOREIGN KEY (`costumer_id`)
-    REFERENCES `moon`.`costumer` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_payment_costumer_andress1`
-    FOREIGN KEY (`costumer_andress_id`)
-    REFERENCES `moon`.`costumer_andress` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_payment_costumer_card1`
-    FOREIGN KEY (`costumer_card_id`)
-    REFERENCES `moon`.`costumer_card` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `moon`.`costumer` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -51,8 +13,16 @@ CREATE TABLE IF NOT EXISTS `moon`.`costumer` (
   `phone_number` INT(11) NOT NULL,
   `birth_date` VARCHAR(100) NOT NULL,
   `document_number` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
+  `costumer_andress_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_costumer_costumer_andress1_idx` (`costumer_andress_id` ASC) VISIBLE,
+  CONSTRAINT `fk_costumer_costumer_andress1`
+    FOREIGN KEY (`costumer_andress_id`)
+    REFERENCES `moon`.`costumer_andress` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
+AUTO_INCREMENT = 2
 DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `moon`.`costumer_andress` (
@@ -66,9 +36,10 @@ CREATE TABLE IF NOT EXISTS `moon`.`costumer_andress` (
   `zipcode` INT(11) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 2
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `moon`.`costumer_card` (
+CREATE TABLE IF NOT EXISTS `moon`.`payment_card` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `method` VARCHAR(150) NOT NULL,
   `number` VARCHAR(255) NOT NULL,
@@ -79,22 +50,42 @@ CREATE TABLE IF NOT EXISTS `moon`.`costumer_card` (
   `expiration_year` INT(11) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 2
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `moon`.`item` (
+CREATE TABLE IF NOT EXISTS `moon`.`payment_item` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(255) NULL DEFAULT NULL,
   `quantity` INT(11) NULL DEFAULT NULL,
   `price_in_cents` INT(11) NULL DEFAULT NULL,
   `payment_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_item_payment1_idx` (`payment_id` ASC),
+  INDEX `fk_item_payment1_idx` (`payment_id` ASC) VISIBLE,
   CONSTRAINT `fk_item_payment1`
     FOREIGN KEY (`payment_id`)
-    REFERENCES `moon`.`payment` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `moon`.`payment` (`id`))
 ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `moon`.`payment` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `unique_id` VARCHAR(200) NOT NULL,
+  `currency` VARCHAR(45) NOT NULL,
+  `value_in_cents` INT(11) NOT NULL,
+  `installments` INT(11) NOT NULL,
+  `costumer_id` INT(11) NOT NULL,
+  `costumer_card_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_payment_costumer_idx` (`costumer_id` ASC) VISIBLE,
+  INDEX `fk_payment_costumer_card1_idx` (`costumer_card_id` ASC) VISIBLE,
+  CONSTRAINT `fk_payment_costumer`
+    FOREIGN KEY (`costumer_id`)
+    REFERENCES `moon`.`costumer` (`id`),
+  CONSTRAINT `fk_payment_costumer_card1`
+    FOREIGN KEY (`costumer_card_id`)
+    REFERENCES `moon`.`payment_card` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
 DEFAULT CHARACTER SET = utf8;
 
 
